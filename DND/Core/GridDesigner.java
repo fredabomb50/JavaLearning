0209dcc8c9c28a9e9a3c097855a0ac9148cb0c57
Luis J Pacheco <luisjpacheco50@gmail.com>
2022-02-25 15:16:02 -0500

    Update GridDesigner.java
    minor changes to format; added comment sections for future constraint types.

--- DND/Core/GridDesigner.java
+++ DND/Core/GridDesigner.java
@@ -1,10 +1,34 @@
 package core;
 
+import java.awt.GridBagConstraints;
+
 public class GridDesigner extends Gui
 {
-	//======================Constants for uniform Grid design
+	//======================Constants
 	//Defaults
-	final private static int button_DefaultGridWidth = 1;
-	final private static int button_DefaultGridHeight = 1;
-	final private static int label_DefaultSize = 1;
+	final private static int bttn_DefaultGridWidth = 1;
+	final private static int bttn_DefaultGridHeight = 1;
+	final private static int lbl_DefaultSize = 1;
+	
+	
+	//======================Variables
+	//Labels
+	private GridBagConstraints cClassLabel, cLevelLabel; 
+	
+	//Panels
+	private GridBagConstraints cAlignBGPanel, cAvatarPanel; 
+	
+	//Buttons
+	private GridBagConstraints cCharacterDetailsButton;
+	
+	//
+	
+	
+	//======================Constructor
+	public GridDesigner()
+	{
+		
+	}
+	
+	
 }
